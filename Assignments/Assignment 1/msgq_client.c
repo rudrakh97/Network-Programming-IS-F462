8d619219965f372c2f26b5d8654d9077ce3c391a
rudrakh97 <rudrakh97@gmail.com>
2019-02-19 00:12:27 +0530

    Added joining and messaging functionalities

--- Assignments/Assignment 1/msgq_client.c
+++ Assignments/Assignment 1/msgq_client.c
@@ -16,9 +16,9 @@
 #define MAX_USERNAME_SIZE 20
 #define MAX_GROUPS 10
 
-mqd_t server, uclient;
+mqd_t server, uclient, client;
 
-char in_buffer[MAX_MSG_SIZE+10], out_buffer[MAX_MSG_SIZE+10];
+char in_buffer[MAX_MSG_SIZE+10], out_buffer[MAX_MSG_SIZE+10], clientq[15], pid[15];
 
 int open_server()
 {
@@ -46,6 +46,31 @@ int open_uclient()
 
 }
 
+void set_clientq()
+{
+	memset(clientq, '\0', sizeof(clientq));
+	sprintf(clientq,"/%dq",getpid());
+	memset(pid, '\0', sizeof(pid));
+	sprintf(pid," %d",getpid());
+}
+
+int open_client()
+{
+	struct mq_attr attr;
+	attr.mq_flags = 0;
+	attr.mq_maxmsg = MAX_MSGS;
+	attr.mq_msgsize = MAX_MSG_SIZE;
+	attr.mq_curmsgs = 0;
+	set_clientq();
+	if((client = mq_open(clientq, O_RDONLY | O_CREAT, PERMISSIONS, &attr)) == -1)
+	{
+		perror(">> Client start error");
+		return 1;
+	}
+	printf(">> Client open successful: %s ...\n", clientq);
+	return 0;
+}
+
 int read_uclient_queue()
 {
 	if((mq_receive(uclient, in_buffer, MAX_MSG_SIZE+10, NULL)) == -1)
@@ -58,8 +83,21 @@ int read_uclient_queue()
 	return 0;
 }
 
+int read_client_queue()
+{
+	if((mq_receive(client, in_buffer, MAX_MSG_SIZE+10, NULL)) == -1)
+	{
+		perror(">> Client queue read error");
+		return 1;
+	}
+	printf(">> ");
+	puts(in_buffer);
+	return 0;
+}
+
 int send_server_queue()
 {
+	strcat(out_buffer, pid);
 	if((mq_send(server, out_buffer, strlen(out_buffer)+1, 0)) == -1)
 	{
 		perror(">> Server queue write error");
@@ -75,6 +113,8 @@ void exit_handler(int sig_no)
 	mq_unlink(SERVER);
 	mq_close(uclient);
 	mq_unlink(U_CLIENT);
+	mq_close(client);
+	mq_unlink(clientq);
 	printf("\n>> Client exited\n");
 	signal(sig_no, SIG_DFL);
 	raise(sig_no);
@@ -91,14 +131,30 @@ int main()
 		exit(1);
 	if( open_uclient() > 0 )
 		exit(1);
-	while(1)
+	if( open_client() > 0 )
+		exit(1);
+
+	int fpid = fork();
+	if(fpid == 0)
 	{
-		memset(out_buffer, '\0', sizeof(out_buffer));
-		printf(">> ");
-		gets(out_buffer);
-		send_server_queue();
-		memset(in_buffer, '\0', sizeof(in_buffer));
-		read_uclient_queue();
+		while(1)
+		{
+			memset(out_buffer, '\0', sizeof(out_buffer));
+			printf(">> ");
+			gets(out_buffer);
+			send_server_queue();
+			memset(in_buffer, '\0', sizeof(in_buffer));
+			if(out_buffer[0] == '!' && out_buffer[1] == 'l' && out_buffer[2] == 'i' && out_buffer[3] == 's' && out_buffer[4] == 't') // !list
+			{
+				read_uclient_queue();
+				continue;
+			}
+		}
+	}
+	else
+	{
+		while(1)
+			read_client_queue();
 	}
 	return 0;
 }
--- Assignments/Assignment 1/msgq_server.c
+++ Assignments/Assignment 1/msgq_server.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <mqueue.h>
@@ -16,11 +17,11 @@
 #define MAX_USERNAME_SIZE 20
 #define MAX_GROUPS 10
 
-mqd_t server, uclient;
+mqd_t server, uclient, client;
 
 int user_lookup[MAX_USERS];
 int lookup_matrix[MAX_GROUPS][MAX_USERS];
-char in_buffer[MAX_MSG_SIZE+10], out_buffer[MAX_MSG_SIZE+10];
+char in_buffer[MAX_MSG_SIZE+10], out_buffer[MAX_MSG_SIZE+10], clientq[15];
 
 int open_server()
 {
@@ -62,6 +63,7 @@ int open_uclient()
 
 int read_server_queue()
 {
+	memset(in_buffer, '\0', sizeof(in_buffer));
 	if((mq_receive(server, in_buffer, MAX_MSG_SIZE+10, NULL)) == -1)
 	{
 		perror(">> Server queue read error");
@@ -81,9 +83,28 @@ int send_uclient_queue()
 	return 0;
 }
 
-int send_client_queue(int client_id)
+void set_clientq(int client_pid)
+{
+	memset(clientq, '\0', sizeof(clientq));
+	sprintf(clientq,"/%dq",client_pid);
+}
+
+int send_client_queue(int client_pid)
 {
 	// TO DO
+	set_clientq(client_pid);
+	printf(">> Opening client queue: %s\n", clientq);
+	if((client = mq_open(clientq, O_WRONLY)) == -1)
+	{
+		perror("Client queue open error");
+		return 1;
+	}
+	if((mq_send(client, out_buffer, strlen(out_buffer)+1,0)) == -1)
+	{
+		perror(">> Client queue write error");
+		return 1;
+	}
+	mq_close(client);
 	return 0;
 }
 
@@ -130,11 +151,56 @@ int list()
 int join()
 {
 	// TO DO
+	memset(out_buffer, '\0', sizeof(out_buffer));
+	int ind = 6;
+	while(isspace(in_buffer[ind]) && in_buffer[ind] != '\0')
+		ind++;
+	int group_id = -1;
+	while(in_buffer[ind] != '\0' && !isspace(in_buffer[ind]))
+	{
+		if(group_id < 0) group_id = 0;
+		group_id *= 10;
+		group_id += (in_buffer[ind]-'0');
+		ind++;
+	}
+	while(isspace(in_buffer[ind]) && in_buffer[ind] != '\0')
+		ind++;
+	printf("%c\n", in_buffer[ind]);
+	int client_pid = 0;
+	while(in_buffer[ind] != '\0' && !isspace(in_buffer[ind]))
+	{
+		client_pid *= 10;
+		client_pid += (in_buffer[ind]-'0');
+		ind++;
+	}
+	printf(">> Adding %d to group id %d...\n", client_pid, group_id);
+	if(group_id < 0 || client_pid == 0)
+	{
+		strcpy(out_buffer, "Couldn't join group. Make sure entered group id is non negative!");
+		return 0;
+	}
+	for(ind=0;ind<MAX_USERS;++ind)
+		if(user_lookup[ind] == 0)
+		{
+			user_lookup[ind] = client_pid;
+			break;
+		}
+	if(ind == MAX_USERS)
+	{
+		strcpy(out_buffer, "Couldn't join group. User limit exceeded!");
+		return 0;
+	}
+	lookup_matrix[group_id][ind] = 1;
+	strcpy(out_buffer, "Joined group!");
+	if(send_client_queue(client_pid) == 1)
+		return 1;
 	return 0;
 }
 
 int message()
 {
+	printf(">> Message recieved: ");
+	puts(in_buffer);
 	if(in_buffer[0] == '!')
 	{
 		if(in_buffer[1] == 'l' && in_buffer[2] == 'i' && in_buffer[3] == 's' && in_buffer[4] == 't') // !list
@@ -156,7 +222,7 @@ int message()
 		return 2;
 	}
 	else if(in_buffer[0] == '@')
-		return 0;
+		return 1;
 	return 2;
 }
 
@@ -193,24 +259,49 @@ int main()
 		if((flag = message()) == 1) // not a request
 		{
 			int group_no = 0, i = 1,j=0;
-			while(in_buffer[i] != ' ' || in_buffer[i] != '\0')
+			while(!isspace(in_buffer[i]) && in_buffer[i] != '\0')
 			{
 				group_no *= 10;
 				group_no += (in_buffer[i]-'0');
 				i++;
 			}
-			i++;
+			while(isspace(in_buffer[i]) != 0 && in_buffer[i] != '\0')
+				i++;
 			while(in_buffer[i]!='\0')
 			{
 				out_buffer[j] = in_buffer[i];
 				i++;
 				j++;
 			}
+			int pow = 1, pid = 0; j = strlen(out_buffer)-1;
+			while(!isspace(out_buffer[j]))
+			{
+				pid += pow*(out_buffer[j]-'0');
+				pow*=10;
+				j--;
+			}
+			printf(">> Relaying message to group %d ...\n", group_no);
+			int legal = 1;
 			for(int i=0;i<MAX_USERS;++i)
 			{
-				if(lookup_matrix[group_no][i] == 1)
+				if(user_lookup[i] == pid)
 				{
-					send_client_queue(i);
+					if(lookup_matrix[group_no][i]==0)
+					{
+						strcpy(out_buffer, "Join group before posting!");
+						send_client_queue(pid);
+						legal = 0;
+						break;
+					}
+				}
+			}
+			if(legal == 0)
+				continue;
+			for(int i=0;i<MAX_USERS;++i)
+			{
+				if(lookup_matrix[group_no][i] == 1 && user_lookup[i] != pid)
+				{
+					send_client_queue(user_lookup[i]);
 				}
 			}
 		}
